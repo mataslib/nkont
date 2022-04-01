213a2b38326d6159452ae97c0b5f4eb2b1ce0bc3
mataslib <matas.lib@gmail.com>
2022-04-01 11:16:32 +0000

    mapa

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import { getWeek } from 'date-fns'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>NKONT</title>
+        <meta name="description" content="NKONT" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <KontImage/>
+      </main>
+        
+    </div>
+  )
+}
+
+const KontImage = () => {
+  const nthWeek = getWeek(new Date(), {
+    weekStartsOn: 1,
+  });
+  const weekString = nthWeek % 2 === 0 ? 'even' : 'odd';
+  console.log(`týden: ${nthWeek} - ${weekString}`);
+
+  return (
+    <div style={{
+      margin: '0 auto',
+      maxWidth: '1240px',
+      padding: '20px'
+    }}>
+      <Image src={`/map${weekString}.jpg`} width="1734px" height="1917px" alt="maka aktuálních kontejnerů" layout="responsive"
+        
+      />
+    </div>
+  )
+}
+
+export default Home
